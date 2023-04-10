149e99b39f56010bdec365d63c0d14e2ce6dcbb0
friday <friday0701@mail.ru>
2023-04-10 13:51:16 +0400

    exepsh1

---
+++ src/main/java/Exep1/hw1.java
@@ -0,0 +1,48 @@
+package Exep1;
+
+// Реализуйте 2 метода, чтобы в каждом из них получить разные исключения
+// Посмотрите на код, и подумайте сколько разных типов исключений вы тут сможете получить?
+
+// Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
+// и возвращающий новый массив, каждый элемент которого равен разности элементов двух входящих массивов в той же ячейке.
+// Если длины массивов не равны, необходимо как-то оповестить пользователя.
+
+// * Реализуйте метод, принимающий в качестве аргументов два целочисленных массива,
+// и возвращающий новый массив, каждый элемент которого равен частному элементов двух входящих массивов в той же ячейке.
+// Если длины массивов не равны, необходимо как-то оповестить пользователя.
+
+// Важно: При выполнении метода единственное исключение, которое пользователь может увидеть - RuntimeException, т.е. ваше.
+public class hw1 {
+    public static void main(String[] args) {
+        int[] arr1 = new int[] {11, 12, 13, 14, 15};
+        int[] arr2 = new int[] {1, 2, 3, 4, 5};
+
+        subtractionArr(arr1, arr2);
+
+        divisionArr(arr1, arr2);
+
+    }
+    public static int[] subtractionArr(int[] array1, int[] array2) {
+        if (array1.length != array2.length) {
+            throw new RuntimeException("Длины массивов не одинаковые");
+        }
+
+        int[] result = new int[];
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i] - array2[i];
+        }
+        return result;
+    }
+
+    public static int[] divisionArr(int[] array1, int[] array2) {
+        if (array1.length != array2.length) {
+            throw new RuntimeException("Длины массивов не одинаковые");
+        }
+
+        int[] result = new int[];
+        for (int i = 0; i < array1.length; i++) {
+            result[i] = array1[i] - array2[i];
+        }
+        return result;
+    }
+}
