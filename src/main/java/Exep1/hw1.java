eb0bb70dc44700ee4deccdc11ef563adfc0f99be
friday <friday0701@mail.ru>
2023-04-10 15:48:56 +0400

    exepsh1.2

--- src/main/java/Exep1/hw1.java
+++ src/main/java/Exep1/hw1.java
@@ -11,25 +11,27 @@ package Exep1;
 // и возвращающий новый массив, каждый элемент которого равен частному элементов двух входящих массивов в той же ячейке.
 // Если длины массивов не равны, необходимо как-то оповестить пользователя.
 
+import java.util.Arrays;
+
 // Важно: При выполнении метода единственное исключение, которое пользователь может увидеть - RuntimeException, т.е. ваше.
 public class hw1 {
     public static void main(String[] args) {
-        int[] arr1 = new int[] {11, 12, 13, 14, 15};
+        int[] arr1 = new int[] {11, 12, 13, 14, 15, 16};
         int[] arr2 = new int[] {1, 2, 3, 4, 5};
 
-        printArr(subtractionArr(arr1, arr2));
+        System.out.println(Arrays.toString(subtractionArr(arr1, arr2)));
 
-    //    printArr(divisionArr(arr1, arr2));
+    //    System.out.println(Arrays.toString(divisionArr(arr1, arr2)));
 
     }
     public static int[] subtractionArr(int[] array1, int[] array2) {
         if (array1.length != array2.length) {
-            throw new RuntimeException("Длины массивов не одинаковые");
+            throw new RuntimeException(String.format("Длина массива 1: %d, длина массива 2: %d.", array1.length, array2.length));
         }
 
         int[] result = new int[array1.length];
         for (int i = 0; i < array1.length; i++) {
-            result[i] = array1[i] - array2[i];
+            result[i] = array1[i] + array2[i];
         }
         return result;
     }
@@ -46,9 +48,4 @@ public class hw1 {
 //        return result;
 //    }
 
-    public static void printArr(int[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(" " + array[i]);
-        }
-    }
 }
